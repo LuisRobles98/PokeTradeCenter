6863d7ea5ca977218f75a172fec41631a713c7a3
luisroblessorroche <luis.roblessorroche@alum.uca.es>
2026-09-01 14:26:30 +0200

    subida

--- poketrade-center/poketrade-center/src/main/java/com/poketradecenter/Service/implementaciones/CartaIntercambioService.java
+++ poketrade-center/poketrade-center/src/main/java/com/poketradecenter/Service/implementaciones/CartaIntercambioService.java
@@ -1,7 +1,6 @@
 package com.poketradecenter.Service.implementaciones;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.poketradecenter.Clase.CartaIntercambio;
